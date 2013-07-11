0bfc423b9c1494cd6e9aee654caad05a7c0385d6
alexesba <alejandro.espinoza@tangosource.com>
2013-07-11 01:04:29 -0500

    allow to  edit and update an event using the same partial form
    [ci skip]

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,12 +1,14 @@
 class EventsController < ApplicationController
 
   def index
+    @events = Event.all
   end
 
   def new
   end
 
   def edit
+    @event = Event.find(params[:id])
   end
 
   def show
@@ -16,9 +18,22 @@ class EventsController < ApplicationController
   end
 
   def update
+    @event = Event.find(params[:id])
+    if @event.update_attributes(event_params)
+      flash[:notice] = "Event updated successfully" 
+      redirect_to events_path
+    else
+      flash[:notice] = "Event wasn't updated as expected" 
+      redirect_to :back 
+    end
   end
 
   def destroy
+  end
+
+  private
+  def event_params
+    params.require(:event).permit(:name, :details, :location)
   end
 
 
