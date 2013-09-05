412f1a90e54b035deef40a9e418c24fb89447b99
Fernando Castellanos <fernando@billfloat.com>
2013-09-04 22:29:37 -0500

    Fix add missing schema information for event model / table

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20130711041442) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: true do |t|
+    t.string   "name"
+    t.text     "details"
+    t.integer  "user_id"
+    t.string   "latitude"
+    t.string   "longitude"
+    t.string   "location_name"
+    t.datetime "scheduled_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
