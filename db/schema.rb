e1bdd5fb362de04196c1315554307cb5321dc0f5
Ignacio Galindo + Sergio Morales <dev+ig+sm@hackd.mx>
2013-09-04 23:23:06 -0500

    Create user model
    [ci skip]

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130711041442) do
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+ActiveRecord::Schema.define(version: 20130905041409) do
 
   create_table "events", force: true do |t|
     t.string   "name"
@@ -24,6 +21,14 @@ ActiveRecord::Schema.define(version: 20130711041442) do
     t.string   "longitude"
     t.string   "location_name"
     t.datetime "scheduled_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "handle"
+    t.string   "email"
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
