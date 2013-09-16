2fc53d8b999a92161d6054e7007ee7996e077f54
Victor Velazquez <velazquezgaspar16@hotmail.com>
2013-09-16 18:01:45 -0500

    Usage of the annotate gem in the models and routes

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: events
+#
+#  id            :integer          not null, primary key
+#  name          :string(255)
+#  details       :text
+#  user_id       :integer
+#  latitude      :string(255)
+#  longitude     :string(255)
+#  location_name :string(255)
+#  scheduled_at  :datetime
+#  created_at    :datetime
+#  updated_at    :datetime
+#
+
 class Event < ActiveRecord::Base
   validates_presence_of :name, :user_id
 
--- config/routes.rb
+++ config/routes.rb
@@ -10,3 +10,18 @@ Hackd::Application.routes.draw do
 
    root to: 'events#index'
 end
+#== Route Map
+# Generated on 16 Sep 2013 18:00
+#
+# github_callback GET    /auth/github/callback(.:format) sessions#create
+#        sessions POST   /sessions(.:format)             sessions#create
+#         session DELETE /sessions/:id(.:format)         sessions#destroy
+#          events GET    /events(.:format)               events#index
+#                 POST   /events(.:format)               events#create
+#       new_event GET    /events/new(.:format)           events#new
+#      edit_event GET    /events/:id/edit(.:format)      events#edit
+#           event GET    /events/:id(.:format)           events#show
+#                 PATCH  /events/:id(.:format)           events#update
+#                 PUT    /events/:id(.:format)           events#update
+#                 DELETE /events/:id(.:format)           events#destroy
+#            root GET    /                               events#index
