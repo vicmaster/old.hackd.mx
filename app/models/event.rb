747561bb4dab271e22477bc742ee7c2b10916af4
Fernando Castellanos <fernando@billfloat.com>
2013-09-16 08:42:47 -0700

    Very basic event form, WIP

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,6 @@
 class Event < ActiveRecord::Base
-  validates_presence_of :name, :details, :user_id
+  validates_presence_of :name, :user_id
+
+  # Note: Make sure that this doesn't conflict with ActiveRecord
+  attr_accessor :where
 end
