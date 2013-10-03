7764419510adeca7e05954bd8022b6b33fc44a1b
Ignacio Galindo <joigama@gmail.com>
2013-10-03 01:49:44 -0500

    Remove weird message from database schema
    [ci skip]

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20130919043416) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "events", force: true do |t|
     t.string   "name"
     t.text     "details"
