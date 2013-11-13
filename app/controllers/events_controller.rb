27bfcf610eab90ec0665ed719f3c8dc0b22d7173
Victor Velazquez <victor.velazquez@crowdint.com>
2013-11-13 02:56:34 -0600

    Remove action in update method when fails
    * Avoiding redundancy
    * Using the path helper is breaking the test

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -34,7 +34,7 @@ class EventsController < ApplicationController
       redirect_to events_path
     else
       flash[:notice] = "Event wasn't updated as expected"
-      render action: 'edit'
+      render :edit
     end
   end
 
